2d5ca446457394a54e4444de4c4e48e1db3c1398
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-06-27 07:40:23 -0700

    intro complete

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,9 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, TextInput, Button } from 'react-native';
 import styles from './IntroStyle';
+
+// import logo from "../../assets/logo.png"
 
 // create a component
 function Intro ()  {
@@ -9,12 +11,28 @@ function Intro ()  {
     <View style={styles.container}>
       <View style={styles.one}>
         <Text>Hello 1</Text>
+        <Image
+          source={require('../../assets/logo.png')}
+          style={{height: 72, width: 302, position: 'absolute'}}
+        />
       </View>
       <View style={styles.two}>
-        <Text>Hello 1</Text>
+        <TextInput
+          placeholderTextColor="white"
+          placeholder="carreo electronico"
+          style={styles.inp1}
+        />
+        <TextInput
+          placeholderTextColor="white"
+          placeholder="contrasefia"
+          style={styles.inp1}
+        />
+        <View>
+          <Text style={styles.text}>¿Olvidaste tu contraseña? </Text>
+        </View>
       </View>
       <View style={styles.three}>
-        <Text>Hello 1</Text>
+        <Button title="Login" style={styles.logbtn}/>
       </View>
     </View>
   );
