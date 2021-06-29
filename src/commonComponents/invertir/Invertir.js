7d74e2f88a58d072d61573063032a96b6675d6d3
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-06-29 13:42:25 -0700

    complete

--- src/commonComponents/invertir/Invertir.js
+++ src/commonComponents/invertir/Invertir.js
@@ -1,25 +1,81 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text,  } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
 
 import InvertirStyles from './InvertirStyle';
 
 
-// create a component
-function Invertir(){
-    return (
-      <View style={InvertirStyles.container}>
-        <View style={InvertirStyles.parent}>
-          <Text>hello</Text>
-        </View>
-        <View style={InvertirStyles.paren}>
-          <Text>hello1</Text>
-        </View>
-      </View>
-    );
+const image = {
+  uri: 'http://www.chifudesign.com/wp-content/uploads/2019/10/1569870187_maxresdefault.jpg',
 };
 
+// create a component
+function Invertir() {
+  return (
+    <View style={InvertirStyles.container}>
+      <View style={InvertirStyles.parent}>
+        <View style={InvertirStyles.topChild}>
+          <View style={InvertirStyles.topChildL}>
+            <Text style={{fontWeight: 'bold', fontSize: 22, padding: 7}}>
+              Desarrollo Alfa
+            </Text>
+            <Text style={{marginLeft: 7}}>Por Desarrollo 1</Text>
+          </View>
+          <View style={InvertirStyles.topChildR}>
+            <Text style={{paddingRight: 15, fontWeight: 'bold'}}>
+              Quedan 39 dias
+            </Text>
+          </View>
+        </View>
 
+        <View style={InvertirStyles.mdlChild}>
+          <Text
+            style={{
+              flex: 0.17,
+              backgroundColor: '#227BFF',
+              color: '#FFFFFF',
+              textAlign: 'center',
+              justifyContent: 'center',
+              fontSize: 18,
+            }}>
+            Deuda
+          </Text>
+          <View style={{flex: 1.08}}>
+            <Image source={image} style={{height: "100%", width:"100%"}}/>
+          </View>
+          <View style={{flex: 0.09, backgroundColor: '#227BFF'}}></View>
+        </View>
+        <View style={InvertirStyles.mdl2Child}>
+          <View style={InvertirStyles.mdl2Childtxt}>
+            <Text>$ 1.5MM recaudado</Text>
+          </View>
+          <View style={InvertirStyles.mdl2Childtxt}>
+            <Text>Min $ 4.5MM</Text>
+          </View>
+          <View style={InvertirStyles.mdl2Childtxt}>
+            <Text>Max $ 4.5MM</Text>
+          </View>
+        </View>
+        <View style={InvertirStyles.btmChild}>
+          <View style={InvertirStyles.btmChildtxt}>
+            <Text style={{fontSize: 18}}>Tasa anual fija</Text>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>12.32 %</Text>
+          </View>
+          <View style={InvertirStyles.btmChildtxt}>
+            <Text style={{fontSize: 18}}>Min de inversion</Text>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>$ 54,000 MXN</Text>
+          </View>
+          <View style={InvertirStyles.btmChildtxt}>
+            <Text style={{fontSize: 18}}>Plazo</Text>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>12 meses</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={InvertirStyles.paren}></View>
+    </View>
+  );
+}
 
 //make this component available to the app
 export default Invertir;
--- src/commonComponents/invertir/InvertirStyle.js
+++ src/commonComponents/invertir/InvertirStyle.js
@@ -6,23 +6,82 @@ const InvertirStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
+    // backgroundColor: 'pink',
   },
   parent: {
-    flex: 2,
+    flex: 5,
     backgroundColor: '#FFFFFF',
-    width: "90%",
+    width: '95%',
     margin: 30,
-   
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
 
+    elevation: 12,
   },
   paren: {
     flex: 3,
-    backgroundColor: 'red',
-    width: "90%",
-    
-
+    // backgroundColor: 'red',
+    width: '90%',
   },
+  topChild: {
+    flex: 1.5,
+    backgroundColor: '#FFFFFF',
+    width: '100%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    flexDirection: 'row',
+  },
+  mdlChild: {
+    flex: 3.5,
+    // backgroundColor: 'black',
+    width: '100%',
+  },
+  mdl2Child: {
+    flex: 1,
+    flexDirection: 'row',
+    // backgroundColor: 'pink',
+    width: '100%',
+
+    borderBottomWidth: 1,
+  },
+  btmChild: {
+    flex: 2,
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  topChildL: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  topChildR: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  mdl2Childtxt: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 5,
+    fontSize: 18,
+  },
+  btmChildtxt:{
+      flex: 1,
+      justifyContent: "center",
+      paddingLeft: 10,
+      
+
+  }
 });
+
 
 export default InvertirStyles;
